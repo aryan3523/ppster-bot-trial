38448e7013d241c387f0fd6409a5fafe2432c4a3
aryan3523 <145961802+aryan3523@users.noreply.github.com>
2024-09-25 18:19:29 +0530

    Create web.py

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+web = Flask(name)
+
+# Define a route for the homepage
+.route('/')
+def hello_world():
+    return 'This bot is made by AssassiN and currently it hosted and live for everyone'
+
+# Run the application
+if name == 'main':
+  web()
