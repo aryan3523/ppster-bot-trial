3a8f6e26607e3c0980e9fd735106e3ae02a60128
aryan3523 <145961802+aryan3523@users.noreply.github.com>
2024-09-25 18:27:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from pyrogram import Client, filters
-from 
+from prime import *
+from bookmyshow import *
 # Replace with your own API ID and API Hash
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "9590156"
+API_HASH = "368a346bb1b206b650f2b3b37f91e237"
+BOT_TOKEN = "7346471184:AAGerFMfoKR2MljNmWIuZdj9PIlVgPifkhw"
 
 # Create the bot client
 app = Client("my_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
@@ -20,10 +21,32 @@ def handle_ask(client, message):
         message.reply(org_text)
     else:
         message.reply("Please write your query along with /ask command.")
-        
-@app.on_message(filters.text)
-def echo(client, message):
-    message.reply(f"You said: {message.text}")
+ 
+@app.on_message(filters.command("amzn"))
+async def handle_amzn_command(client, message):
+    if len(message.command) < 2:
+        await message.reply_text("Please provide a valid Amazon Prime Video URL after the /amzn command.")
+        return
+    
+    url = message.command[1]
+    if "primevideo.com" in url:
+        covershot_url = scrape_amazon_prime_covershot(url)
+        await message.reply_text(covershot_url)
+    else:
+        await message.reply_text("Please send a valid Amazon Prime Video URL.")
+
+@app.on_message(filters.command("bms"))
+async def handle_bms_command(client, message):
+    if len(message.command) < 2:
+        await message.reply_text("Please provide a valid BMS URL after the /bms command.")
+        return
+    
+    url = message.command[1]
+    if "bookmyshow" in url:
+        poster_url = bms_poster(url)
+        await message.reply_text(poster_url)
+    else:
+        await message.reply_text("Please send a valid BookMyShow URL.")                
 
 if __name__ == "__main__":
     app.run()
